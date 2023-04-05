71feb0048fafd8aefb04b41825c1c97c4f312e57
“chanoo” <x64core@gmail.com>
2023-04-05 11:09:06 +0900

    이메일 유효성 검사 추가

---
+++ KlockApp/Utils/Validators/EmailValidator.swift
@@ -0,0 +1,21 @@
+//
+//  EmailValidator.swift
+//  KlockApp
+//
+//  Created by 성찬우 on 2023/03/30.
+//
+
+import Foundation
+
+struct EmailValidator {
+    private let emailPattern = RegexConstants.emailPattern
+    private let emailPredicate: NSPredicate
+
+    init() {
+        emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailPattern)
+    }
+
+    func isValid(_ email: String) -> Bool {
+        return emailPredicate.evaluate(with: email)
+    }
+}
