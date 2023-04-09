b96923fb14dcdf8f740e19498fe907cb5677a07c
성찬우 <fastchanoo@gmail.com>
2023-04-09 14:34:09 +0900

    화면 들어 오자마자 키보드 입력이 바로 되도록 수정

--- KlockApp/Views/Components/FancyTextField.swift
+++ KlockApp/Views/Components/FancyTextField.swift
@@ -7,31 +7,70 @@
 
 import SwiftUI
 
+struct FancyTextFieldWrapper: UIViewRepresentable {
+    let placeholder: String
+    @Binding var text: String
+    let keyboardType: UIKeyboardType
+    let isSecureField: Bool
+    @Binding var firstResponder: Bool
+
+    init(placeholder: String, text: Binding<String>, keyboardType: UIKeyboardType = .default, isSecureField: Bool = false, firstResponder: Binding<Bool>) {
+        self.placeholder = placeholder
+        self._text = text
+        self.keyboardType = keyboardType
+        self.isSecureField = isSecureField
+        self._firstResponder = firstResponder
+    }
+
+    func makeUIView(context: Context) -> UITextField {
+        let textField = UITextField()
+        textField.keyboardType = keyboardType
+        textField.isSecureTextEntry = isSecureField
+        textField.delegate = context.coordinator
+        textField.addTarget(context.coordinator, action: #selector(Coordinator.textFieldDidChange(_:)), for: .editingChanged)
+        textField.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        return textField
+    }
+
+    func updateUIView(_ uiView: UITextField, context: Context) {
+        uiView.text = text
+        uiView.placeholder = placeholder
+
+        if firstResponder {
+            uiView.becomeFirstResponder()
+        } else {
+            uiView.resignFirstResponder()
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UITextFieldDelegate {
+        var fancyTextFieldWrapper: FancyTextFieldWrapper
+
+        init(_ fancyTextFieldWrapper: FancyTextFieldWrapper) {
+            self.fancyTextFieldWrapper = fancyTextFieldWrapper
+        }
+
+        @objc func textFieldDidChange(_ textField: UITextField) {
+            fancyTextFieldWrapper.text = textField.text ?? ""
+        }
+    }
+}
+
 struct FancyTextField: View {
     let placeholder: String
     @Binding var text: String
     let keyboardType: UIKeyboardType
     let isSecureField: Bool
-
-    init(placeholder: String, text: Binding<String>, keyboardType: UIKeyboardType = .default, isSecureField: Bool = false) {
-        self.placeholder = placeholder
-        self._text = text
-        self.keyboardType = keyboardType
-        self.isSecureField = isSecureField
-    }
+    @Binding var firstResponder: Bool
 
     var body: some View {
-        Group {
-            if isSecureField {
-                SecureField(placeholder, text: $text)
-                    .keyboardType(keyboardType)
-            } else {
-                TextField(placeholder, text: $text)
-                    .keyboardType(keyboardType)
-            }
-        }
-        .padding()
-        .background(Color.white)
-        .cornerRadius(22)
+        FancyTextFieldWrapper(placeholder: placeholder, text: $text, keyboardType: keyboardType, isSecureField: isSecureField, firstResponder: $firstResponder)
+            .padding()
+            .background(Color.white)
+            .cornerRadius(22)
     }
 }
