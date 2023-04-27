8d2891dae2db72ce57dffd73f951a1b48c124b12
성찬우 <fastchanoo@gmail.com>
2023-04-27 17:18:52 +0900

    뒤로가기 안되는거 수정

--- KlockApp/Extensions/UINavigationController+UIGestureRecognizerDelegate.swift
+++ KlockApp/Extensions/UINavigationController+UIGestureRecognizerDelegate.swift
@@ -18,10 +18,6 @@ extension UINavigationController: UIGestureRecognizerDelegate {
 
     // 제스처 인식자가 시작되어야 하는지 결정하는 메소드를 구현합니다.
     public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-        // AppState에서 swipeEnabled를 확인하여 스와이프 백 제스처를 활성화할지 결정합니다.
-        if AppState.shared.swipeEnabled {
-            return viewControllers.count > 1 // 뷰 컨트롤러 스택에 두 개 이상의 뷰 컨트롤러가 있는 경우에만 스와이프 백을 활성화합니다.
-        }
-        return false // 그렇지 않으면 스와이프 백을 비활성화합니다.
+        return viewControllers.count > 1 // 뷰 컨트롤러 스택에 두 개 이상의 뷰 컨트롤러가 있는 경우에만 스와이프 백을 활성화합니다.
     }
 }
