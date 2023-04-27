9d7a4ef142bc58c73d1b5829d8b381e574ce9b90
성찬우 <fastchanoo@gmail.com>
2023-04-27 17:46:20 +0900

    뽀모도로 타이머 리팩토링

--- KlockApp/Views/Screens/Timer/PomodoroTimerView.swift
+++ KlockApp/Views/Screens/Timer/PomodoroTimerView.swift
@@ -22,114 +22,111 @@ struct PomodoroTimerView: View {
     }
     
     var body: some View {
-        ZStack {
-            // Front view
-            VStack {
-                HStack {
-                    Spacer()
+        GeometryReader { geometry in
+            frontView(geometry: geometry)
+                .rotation3DEffect(.degrees(isFlipped ? 180 : 0), axis: (x: 0.0, y: 1.0, z: 0.0))
+                .opacity(isFlipped ? 0 : 1)
+            
+            backView(geometry: geometry)
+                .rotation3DEffect(.degrees(isFlipped ? 0 : 180), axis: (x: 0.0, y: 1.0, z: 0.0))
+                .opacity(isFlipped ? 1 : 0)
+        }
+        .background(FancyColor.background.color)
+        .cornerRadius(10)
+        .shadow(color: Color(.systemGray).opacity(0.2), radius: 5, x: 0, y: 0)
+    }
+    
+    private func frontView(geometry: GeometryProxy) -> some View {
+        VStack {
+            HStack {
+                Spacer()
+                Button(action: {
+                    withAnimation(.spring()) {
+                        isFlipped.toggle()
+                    }
+                }) {
+                    Image(systemName: "gearshape")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                        .padding(24)
+                }
+            }
+            Spacer()
+            Text("뽀모도로 타이머")
+            Spacer()
+        }
+    }
+    
+    private func backView(geometry: GeometryProxy) -> some View {
+        VStack {
+            List {
+                Section(header: Text("뽀모도로 설정")) {
+                    VStack(alignment: .leading) {
+                        Text("공부 시간")
+                            .font(.headline)
+                            .foregroundColor(.gray)
+                        Stepper(value: $workTime, in: 5...60, step: 5) {
+                            Text("\(workTime)분")
+                        }
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text("쉬는 시간")
+                            .font(.headline)
+                            .foregroundColor(.gray)
+                        Stepper(value: $breakTime, in: 5...60, step: 5) {
+                            Text("\(breakTime)분")
+                        }
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text("반복 횟수")
+                            .font(.headline)
+                            .foregroundColor(.gray)
+                        Stepper(value: $repeatCount, in: 1...10) {
+                            Text("\(repeatCount)회")
+                        }
+                    }
+                }
+                
+                Section {
+                    Button(action: {
+                        // Save settings
+                        withAnimation(.spring()) {
+                            isFlipped.toggle()
+                        }
+                    }) {
+                        Text("저장")
+                            .frame(maxWidth: .infinity, alignment: .center)
+                    }
+                }
+                
+                Section {
                     Button(action: {
                         withAnimation(.spring()) {
                             isFlipped.toggle()
                         }
                     }) {
-                        Image(systemName: "gearshape")
-                            .resizable()
-                            .frame(width: 20, height: 20)
-                            .padding(24)
+                        Text("취소")
+                            .frame(maxWidth: .infinity, alignment: .center)
                     }
                 }
-                Spacer()
-                Text("뽀모도로 타이머")
-                Spacer()
-            }
-            .background(FancyColor.background.color)
-            .cornerRadius(10)
-            .shadow(color: Color(.systemGray).opacity(0.2), radius: 5, x: 0, y: 0)
-            .rotation3DEffect(
-                .degrees(isFlipped ? 180 : 0),
-                axis: (x: 0.0, y: 1.0, z: 0.0)
-            )
-            .opacity(isFlipped ? 0 : 1)
-
-            // Back view (settings)
-            VStack {
-                List {
-                    Section(header: Text("뽀모도로 설정")) {
-                        VStack(alignment: .leading) {
-                            Text("공부 시간")
-                                .font(.headline)
-                                .foregroundColor(.gray)
-                            Stepper(value: $workTime, in: 5...60, step: 5) {
-                                Text("\(workTime)분")
-                            }
+                
+                Section {
+                    Button(action: {
+                        withAnimation(.spring()) {
+                            isFlipped.toggle()
+                            Foast.show(message: "삭제 되었습니다.")
                         }
-                        
-                        VStack(alignment: .leading) {
-                            Text("쉬는 시간")
-                                .font(.headline)
-                                .foregroundColor(.gray)
-                            Stepper(value: $breakTime, in: 5...60, step: 5) {
-                                Text("\(breakTime)분")
-                            }
-                        }
-                        
-                        VStack(alignment: .leading) {
-                            Text("반복 횟수")
-                                .font(.headline)
-                                .foregroundColor(.gray)
-                            Stepper(value: $repeatCount, in: 1...10) {
-                                Text("\(repeatCount)회")
-                            }
-                        }
-                    }
-                    
-                    Section {
-                        Button(action: {
-                            // Save settings
-                            withAnimation(.spring()) {
-                                isFlipped.toggle()
-                            }
-                        }) {
-                            Text("저장")
-                                .frame(maxWidth: .infinity, alignment: .center)
-                        }
-                    }
-                    
-                    Section {
-                        Button(action: {
-                            withAnimation(.spring()) {
-                                isFlipped.toggle()
-                            }
-                        }) {
-                            Text("취소")
-                                .frame(maxWidth: .infinity, alignment: .center)
-                        }
-                    }
-                    
-                    Section {
-                        Button(action: {
-                            withAnimation(.spring()) {
-                                isFlipped.toggle()
-                                Foast.show(message: "삭제 되었습니다.")
-                            }
-                        }) {
-                            Text("삭제")
-                                .frame(maxWidth: .infinity, alignment: .center)
-                                .foregroundColor(FancyColor.secondary.color)
-                        }
+                    }) {
+                        Text("삭제")
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .foregroundColor(FancyColor.secondary.color)
                     }
                 }
-//                .listStyle(GroupedListStyle())
-                .clearListBackground()
             }
-            .background(FancyColor.background.color)
-            .cornerRadius(10)
-            .shadow(color: Color(.systemGray).opacity(0.2), radius: 5, x: 0, y: 0)
-            .rotation3DEffect(
-                .degrees(isFlipped ? 0 : 180),
-                axis: (x: 0.0, y: 1.0, z: 0.0)
-            )
-            .opacity(isFlipped ? 1 : 0)
+    //                .listStyle(GroupedListStyle())
+            .clearListBackground()
         }
     }
 }
