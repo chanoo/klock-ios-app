687a232e5b8d3ce4379e5692127b11366aa3a3d6
성찬우 <fastchanoo@gmail.com>
2023-05-15 18:34:40 +0900

    Splash 화면 다크 모드 개발 #6

--- KlockApp/Utils/Constants/FancyColor.swift
+++ KlockApp/Utils/Constants/FancyColor.swift
@@ -14,6 +14,9 @@ enum FancyColor: String {
     case background = "color_background"
     case gray9 = "color_gray9"
 
+    case launchBackground = "color_launch_background"
+    case launchSymbol = "color_launch_symbol"
+
     var color: Color {
         Color(self.rawValue)
     }
