37f61777c4a6232e9cd874139dfaee151384e0de
성찬우 <fastchanoo@gmail.com>
2024-02-17 21:39:16 +0900

    LongView의 다크 모드 적용

--- KlockApp/Views/Components/LoadingView.swift
+++ KlockApp/Views/Components/LoadingView.swift
@@ -11,21 +11,24 @@ struct LoadingView: View {
     @Environment(\.colorScheme) var colorScheme
 
     var body: some View {
-        VStack {
+        HStack {
             Spacer()
-
-            ZStack(alignment: .center) {
-                if colorScheme == .dark {
-                    LottieView(name: "lottie-loading-white", speed: 1.1)
-                        .frame(width: 120, height: 120)
-                } else {
-                    LottieView(name: "lottie-loading-black", speed: 1.1)
-                        .frame(width: 120, height: 120)
+            VStack {
+                Spacer()
+                ZStack(alignment: .center) {
+                    if colorScheme == .dark {
+                        LottieView(name: "lottie-loading-white", speed: 1.1)
+                            .frame(width: 120, height: 120)
+                    } else {
+                        LottieView(name: "lottie-loading-black", speed: 1.1)
+                            .frame(width: 120, height: 120)
+                    }
                 }
+                Spacer()
             }
-            
             Spacer()
         }
+        .background(FancyColor.background.color)
     }
 }
 
